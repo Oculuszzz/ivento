058480c0e2c2dc91793e73a3149e0c1861c5b38c
Muhammad Shahir Mokhtar <91410081+Oculuszzz@users.noreply.github.com>
2023-03-16 20:49:38 +0800

    Update TokenRefreshResponse.java
    Added additional user info on TokenRefresh response

--- src/main/java/com/web/springboot/ivento/payload/response/TokenRefreshResponse.java
+++ src/main/java/com/web/springboot/ivento/payload/response/TokenRefreshResponse.java
@@ -3,7 +3,7 @@
  */
 package com.web.springboot.ivento.payload.response;
 
-import java.util.Objects;
+import java.time.LocalDateTime;
 
 /**
  * @author mokht
@@ -11,32 +11,56 @@ import java.util.Objects;
  */
 public class TokenRefreshResponse {
 
-	private String tokenType = "Bearer";
 	private String accessToken;
+
 	private String refreshToken;
+
+	/**
+	 * HTTP response type - Bearer authentication/token authentication
+	 */
+	private String tokenType = "Bearer";
+
+	private Long id;
+
+	private String username;
+
+	private String email;
+
+	private Boolean isBlocked;
+
+	private String image;
+
+	private String role;
+
+	private LocalDateTime lastLoggedIn;
+
+	private LocalDateTime lastUpdated;
 
 	/**
 	 * @param accessToken
 	 * @param refreshToken
+	 * @param id
+	 * @param username
+	 * @param email
+	 * @param isBlocked
+	 * @param image
+	 * @param role
+	 * @param lastLoggedIn
+	 * @param lastUpdated
 	 */
-	public TokenRefreshResponse(String accessToken, String refreshToken) {
+	public TokenRefreshResponse(String accessToken, String refreshToken, Long id, String username, String email,
+			Boolean isBlocked, String image, String role, LocalDateTime lastLoggedIn, LocalDateTime lastUpdated) {
 		super();
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
-	}
-
-	/**
-	 * @return the tokenType
-	 */
-	public String getTokenType() {
-		return tokenType;
-	}
-
-	/**
-	 * @param tokenType the tokenType to set
-	 */
-	public void setTokenType(String tokenType) {
-		this.tokenType = tokenType;
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.isBlocked = isBlocked;
+		this.image = image;
+		this.role = role;
+		this.lastLoggedIn = lastLoggedIn;
+		this.lastUpdated = lastUpdated;
 	}
 
 	/**
@@ -54,6 +78,132 @@ public class TokenRefreshResponse {
 	}
 
 	/**
+	 * @return the tokenType
+	 */
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	/**
+	 * @param tokenType the tokenType to set
+	 */
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the isBlocked
+	 */
+	public Boolean getIsBlocked() {
+		return isBlocked;
+	}
+
+	/**
+	 * @param isBlocked the isBlocked to set
+	 */
+	public void setIsBlocked(Boolean isBlocked) {
+		this.isBlocked = isBlocked;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the role
+	 */
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	/**
+	 * @return the lastLoggedIn
+	 */
+	public LocalDateTime getLastLoggedIn() {
+		return lastLoggedIn;
+	}
+
+	/**
+	 * @param lastLoggedIn the lastLoggedIn to set
+	 */
+	public void setLastLoggedIn(LocalDateTime lastLoggedIn) {
+		this.lastLoggedIn = lastLoggedIn;
+	}
+
+	/**
+	 * @return the lastUpdated
+	 */
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+
+	/**
+	 * @param lastUpdated the lastUpdated to set
+	 */
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+	/**
 	 * @return the refreshToken
 	 */
 	public String getRefreshToken() {
@@ -65,30 +215,6 @@ public class TokenRefreshResponse {
 	 */
 	public void setRefreshToken(String refreshToken) {
 		this.refreshToken = refreshToken;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(accessToken, refreshToken, tokenType);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TokenRefreshResponse other = (TokenRefreshResponse) obj;
-		return Objects.equals(accessToken, other.accessToken) && Objects.equals(refreshToken, other.refreshToken)
-				&& Objects.equals(tokenType, other.tokenType);
-	}
-
-	@Override
-	public String toString() {
-		return "TokenRefreshResponse [tokenType=" + tokenType + ", accessToken=" + accessToken + ", refreshToken="
-				+ refreshToken + "]";
 	}
 
 }
