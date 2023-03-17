da37f74f0c6ff151b946613b12511fb004cdade4
Muhammad Shahir Mokhtar <91410081+Oculuszzz@users.noreply.github.com>
2023-03-18 01:48:57 +0800

    Create DateUtils.java

---
+++ src/main/java/com/web/springboot/ivento/component/utils/DateUtils.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.web.springboot.ivento.component.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.web.springboot.ivento.controller.CustomerOrderController;
+
+/**
+ * @author mokht
+ *
+ */
+@Component
+public class DateUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(CustomerOrderController.class);
+
+	public LocalDateTime convertIsoDateTimeStringToLocalDateTime(String isoDateTime) {
+
+		LocalDateTime localDateTime = null;
+
+		try {
+
+			localDateTime = LocalDateTime.parse(isoDateTime, DateTimeFormatter.ISO_DATE_TIME);
+
+		} catch (Exception e) {
+
+			logger.error(String.format(
+					"convertIsoDateTimeStringToLocalDateTime(): Failed parse isoDateTime string (%s) to LocalDateTime - Error - %s",
+					isoDateTime, e.getMessage()));
+
+		}
+
+		return localDateTime;
+
+	}
+
+}
