4a65fd2e7b76e4be7fb535c22338917c7ab258eb
Muhammad Shahir Mokhtar <91410081+Oculuszzz@users.noreply.github.com>
2023-03-23 13:10:51 +0800

    Update UserException.java
    Added Response Status annotation

--- src/main/java/com/web/springboot/ivento/service/exception/UserException.java
+++ src/main/java/com/web/springboot/ivento/service/exception/UserException.java
@@ -1,9 +1,13 @@
 package com.web.springboot.ivento.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author mokht
  *
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserException extends RuntimeException {
 
 	/**
